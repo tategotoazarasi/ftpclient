190ee0e37ff15d5031e5c5ef0c8f3622c31cf8fa
Wang Zhiheng <2724167997@qq.com>
2024-03-26 16:21:46 +0800

    Use PrettyTime to format human-readable date and time
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- src/main/java/cn/jsou/ftpclient/utils/TimeUtils.java
+++ src/main/java/cn/jsou/ftpclient/utils/TimeUtils.java
@@ -1,34 +1,22 @@
 package cn.jsou.ftpclient.utils;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class TimeUtils {
-	public static String formatRelativeTime(LocalDateTime dateTime) {
-		LocalDateTime now     = LocalDateTime.now();
-		long          seconds = ChronoUnit.SECONDS.between(dateTime, now);
-		long          minutes = ChronoUnit.MINUTES.between(dateTime, now);
-		long          hours   = ChronoUnit.HOURS.between(dateTime, now);
-		long          days    = ChronoUnit.DAYS.between(dateTime, now);
-		long          weeks   = ChronoUnit.WEEKS.between(dateTime, now);
-		long          months  = ChronoUnit.MONTHS.between(dateTime, now);
+	private static final PrettyTime p = new PrettyTime(new Locale("zh_CN"));
 
-		if (seconds < 60) {
-			return "刚刚";
-		} else if (minutes < 60) {
-			return minutes + "分钟前";
-		} else if (hours < 24) {
-			return hours + "小时前";
-		} else if (days < 7) {
-			return days + "天前";
-		} else if (weeks < 4) {
-			return weeks + "星期前";
-		} else if (months < 12) {
-			return months + "个月前";
-		} else {
-			// 对于超过30天的，直接返回具体日期
+	public static String formatRelativeTime(LocalDateTime dateTime) {
+		long months = ChronoUnit.MONTHS.between(dateTime, LocalDateTime.now());
+		if ((months > 12)) {
+			// 对于超过1年的，直接返回具体日期
 			return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		} else {
+			return p.format(dateTime);
 		}
 	}
 }
